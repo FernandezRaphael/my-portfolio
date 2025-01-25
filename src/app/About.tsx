d7aaa1169cdf06004fc03ad72537767e05ae0bde
FernandezRaphael <fernandezraphael59@gmail.com>
2025-01-25 23:56:40 +0100

    about update

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -6,31 +6,31 @@ export default function About() {
     return (
         <div className={styles.about}>
             <div className={styles.container}>
-                <div>
+                <div className={styles.containerDiv}>
                     <div className={styles.content}>
                         <h2>Portfolio 3D</h2>
                         <span>2024</span>
                     </div>
                 </div>
-                <div>
+                <div className={styles.containerDiv}>
                     <div className={styles.content}>
                         <h2>Soon</h2>
                         <span>2025</span>
                     </div>
                 </div>
-                <div>
+                <div className={styles.containerDiv}>
                     <div className={styles.content}>
                         <h2>Soon</h2>
                         <span>2025</span>
                     </div>
                 </div>
-                <div>
+                <div className={styles.containerDiv}>
                     <div className={styles.content}>
                         <h2>Soon</h2>
                         <span>2025</span>
                     </div>
                 </div>
-                <div>
+                <div className={styles.containerDiv}>
                     <div className={styles.content}>
                         <h2>Soon</h2>
                         <span>2025</span>
